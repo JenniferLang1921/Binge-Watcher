a0ff4652127d3abadd4146959a92e50f6697e271
ggaeth <34075514+ggaeth@users.noreply.github.com>
2018-05-15 13:26:50 -0500

    indent

--- server.js
+++ server.js
@@ -1,67 +1,78 @@
-var express    = require('express')
-    var app        = express()
-    var passport   = require('passport')
-    var session    = require('express-session')
-    var bodyParser = require('body-parser')
-    var env        = require('dotenv').load()
-    var exphbs     = require('express-handlebars')
+var express = require('express')
+var app = express()
+var passport = require('passport')
+var session = require('express-session')
+var bodyParser = require('body-parser')
+var env = require('dotenv').load()
+var exphbs = require('express-handlebars')
 
 
-    var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
-    //For BodyParser
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(bodyParser.json());
-
-
-     // For Passport
-    app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
-    app.use(passport.initialize());
-    app.use(passport.session()); // persistent login sessions
+//For BodyParser
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 
 
-     //For Handlebars
-
-     app.engine("hbs", exphbs({ defaultLayout: "main"}));
-     app.set("view engine", "hbs");
-
-
-    app.set('views', './views')
-    app.engine('hbs', exphbs({extname: '.hbs'}));
-    app.set('view engine', '.hbs');
-
-
-    app.get('/', function(req, res){
-	  res.send('Welcome!');
-	});
+// For Passport
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
 
 
-	//Models
-    var models = require("./models");
+//For Handlebars
+
+app.engine("hbs", exphbs({
+  defaultLayout: "main"
+}));
+app.set("view engine", "hbs");
+app.use(express.static("public"));
 
 
-    //Routes
-    require("./routes/html-routes.js")(app);
-
-    var authRoute = require('./routes/auth.js')(app,passport);
-
-
-    //load passport strategies
-    require('./config/passport/passport.js')(passport,models.user);
+app.set('views', './views')
+app.engine('hbs', exphbs({
+  extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
 
-    //Sync Database
-   	models.sequelize.sync().then(function(){
-    console.log('Database running')
-
-    }).catch(function(err){
-    console.log(err,"Database error")
-    });
+app.get('/', function(req, res) {
+  res.send('Welcome!');
+});
 
 
+//Models
+var models = require("./models");
 
-	app.listen(3000, function(err){
-		if(!err)
-        console.log("App listening on PORT " + PORT);
 
-	});
+//Routes
+require("./routes/html-routes.js")(app);
+
+var authRoute = require('./routes/auth.js')(app, passport);
+
+
+//load passport strategies
+require('./config/passport/passport.js')(passport, models.user);
+
+
+//Sync Database
+models.sequelize.sync().then(function() {
+  console.log('Database running')
+
+}).catch(function(err) {
+  console.log(err, "Database error")
+});
+
+
+
+app.listen(3000, function(err) {
+  if (!err)
+    console.log("App listening on PORT " + PORT);
+
+});
